62b8eba4b920abef0d210ef756f73f8b1bd2a0a2
fanwenxiaoyu <43022463+fanwenxiaoyu@users.noreply.github.com>
2019-05-20 20:01:39 -0400

    Add files via upload

--- Client-React/src/containers/AddGrade.js
+++ Client-React/src/containers/AddGrade.js
@@ -6,7 +6,7 @@ import { gql } from 'apollo-boost';
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 
 const ADD_STUDENTCOURSE = gql`
-mutation addGrade ($assignmentID: ID!, $studentID: ID!, $grade: String!) {
+mutation addGrade ($assignmentID: ID!, $studentID: ID!, $grade: Float!) {
   createAssignmentGrade(assignmentID: $assignmentID, studentID: $studentID, grade:$grade) {
     id
     assignment {
@@ -38,7 +38,7 @@ class CourseForm extends Component {
   };
 
   validateForm() {
-    return this.state.assignmentID.length > 0 && this.state.grade.length && this.state.studentID.length> 0;
+    return this.state.assignmentID.length > 0 && this.state.grade && this.state.studentID.length> 0;
   }
 
   render() {
@@ -95,7 +95,7 @@ class CourseForm extends Component {
                   <FormControl
                     type="grade"
                     value={this.state.grade}
-                    onChange={e => this.setState({ grade: e.target.value })}
+                    onChange={e => this.setState({ grade: parseFloat(e.target.value)})}
                   />
                 </FormGroup>
 
